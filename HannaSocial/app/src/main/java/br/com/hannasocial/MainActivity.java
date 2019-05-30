49effaf02591d8ad1eed5494f93fcd9413d34a9c
Anônio Dias <acsilvadias@gmail.com>
2019-05-30 03:31:24 -0300

    Sucesso GPS ativado no Hanna

--- HannaSocial/app/src/main/java/br/com/hannasocial/MainActivity.java
+++ HannaSocial/app/src/main/java/br/com/hannasocial/MainActivity.java
@@ -4,24 +4,17 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-<<<<<<< HEAD
+
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.media.MediaMetadataCompatApi21;
-=======
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
->>>>>>> Hanna28052019
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.FocusFinder;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,23 +23,16 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
-<<<<<<< HEAD
-import com.google.android.gms.common.api.PendingResult;
+
+
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsResult;
-=======
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
->>>>>>> Hanna28052019
-import com.google.android.gms.tasks.Task;
 
-import java.nio.file.Paths;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
     /* Verificar GPS*/
@@ -67,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestPermission();
 
         mHandler = new Handler();
         mShowDialog = savedInstanceState == null;
@@ -95,7 +83,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                messageToast(getApplicationContext(), "Não Implementado!", 3);
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+
+                    return;
+                }
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    CharSequence resp;
+                                    resp = "latitude: " + location.getLatitude() + " - " + location.getLongitude();
+                                    // Logic to handle location object
+                                    Log.i("LOG", "latitude: " + location.getLatitude());
+                                    Log.i("LOG", "longitude: " + location.getLongitude());
+                                    messageToast(getApplicationContext(), resp, 5);
+
+                                }
+                            }
+                        });
+
+                //messageToast(getApplicationContext(), "Não Implementado!", 3);
 
             }
         });
@@ -122,32 +133,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            Log.i("LOG", "latitude: " + location.getLatitude());
-                            Log.i("LOG", "longitude: " + location.getLongitude());
-                        }
-                    }
-                });
 
     }
+
+    private  void requestPermission(){
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{ACCESS_FINE_LOCATION},1);
+    }
+
 
     @Override
     public void onSaveInstanceState(Bundle outState){

73dfbeb4e9fededbd3e74742ca43df6c93d9d409
Anônio Dias <acsilvadias@gmail.com>
2019-06-08 19:16:25 -0300

    alteracao no app enviar request

--- HannaBackend/config/database.js
+++ HannaBackend/config/database.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose')
+/* acesso ao atlas
+	user: hannaUserdb
+	psw:  H@nna2019.1
+	atlas: mongodb+srv://hannaUserdb:<password>@cluster0-jryiw.mongodb.net/test?retryWrites=true&w=majority
+ 
+ atlasDB: "mongodb+srv://hannaUserdb:H@nna2019.1@cluster0-jryiw.mongodb.net/db_hannasocial?retryWrites=true&w=majority" 
+local: mongodb://localhost:27017/db_hannasocial
+	*/
 
-module.exports = mongoose.connect("mongodb://localhost:27017/db_hannasocial",
+
+module.exports = mongoose.connect("mongodb://localhost:27017/db_hannasocial" ,
    { useNewUrlParser: true }
    , err => {
     if (err) {
@@ -8,5 +17,5 @@ module.exports = mongoose.connect("mongodb://localhost:27017/db_hannasocial",
       process.exit(1) /*encerar o processo para que o nodemon tente novamente*/
    }
    else{
-	  console.log('DB Hanna Social Connection:OK')  
+     console.log('DB Hanna Social Connection:OK')  
    }}) 

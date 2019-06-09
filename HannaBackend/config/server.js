19622712f8921bfffc3c74aeff7c508b7848ae37
Anônio Dias <acsilvadias@gmail.com>
2019-06-09 02:17:46 -0300

    refatoracao http restful

--- HannaBackend/config/server.js
+++ HannaBackend/config/server.js
@@ -7,8 +7,14 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
+server.use((req, res, next) => {
+	console.log('Datdos recebidos ' + JSON.stringify(req.body, null, 2))
+	next()
+})
 server.listen(port, function() {
 	console.log(`Hanna Backend is running on port ${port}.`)
 })
+
+
 
 module.exports = server

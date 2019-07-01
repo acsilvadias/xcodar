b1c902414550f0b94e4cc297645f1ce8ac223d5c
Anônio Dias <acsilvadias@gmail.com>
2019-07-01 09:52:05 -0300

    implenetando push notification com aws e firebase

--- HannaSocial/app/src/main/java/br/com/hannasocial/DetailActivity.java
+++ HannaSocial/app/src/main/java/br/com/hannasocial/DetailActivity.java
@@ -1,7 +1,7 @@
 package br.com.hannasocial;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
--- HannaSocial/app/src/main/java/br/com/hannasocial/NotificationUtils.java
+++ HannaSocial/app/src/main/java/br/com/hannasocial/NotificationUtils.java
@@ -3,9 +3,9 @@ package br.com.hannasocial;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.app.TaskStackBuilder;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 import android.util.Log;
 
 public class NotificationUtils {
